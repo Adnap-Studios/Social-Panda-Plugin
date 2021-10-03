dec90b81441f352d322b19abb1e95e95500d2530
Marnik Merken <marnik.merken@icloud.com>
2021-10-03 15:49:40 +0200

    Created basic Bungeecord plugin

---
+++ src/main/java/com/adnapstudios/socialpanda/SocialPanda.java
@@ -0,0 +1,11 @@
+package com.adnapstudios.socialpanda;
+
+import net.md_5.bungee.api.plugin.Plugin;
+
+public class SocialPanda extends Plugin {
+
+    @Override
+    public void onEnable() {
+        getLogger().info("[Social Panda Plugin] Plugin loaded!");
+    }
+}
